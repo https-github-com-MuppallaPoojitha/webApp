c3e9e230e01e1b0c75189cab3c6841ba9a67d6bd
muppallapoojitha <muppalla.p@northeastern.edu>
2021-02-11 11:45:01 -0500

    webapp for rest API's

---
+++ src/test/java/com/assingment1/cloud/CloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assingment1.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
